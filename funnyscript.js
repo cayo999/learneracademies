00c3c0e18cb6325adca5d98d77fa34343341dd7e
cayo999 <calebyo1220@gmail.com>
2026-06-07 15:41:10 -0400

    Create funnyscript.js
    so funny guys i laugh hard i cant breath

---
+++ funnyscript.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', (event) => {
+    console.log(`Key pressed: ${event.key}`);
+    if(event.key="``"){
+        window.location.replace("https://www.google.com/search?q=what+time+is+it&rlz=1CAQTNX_enUS1122&oq=what+time&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRg5Mg0IAhAAGJIDGIAEGIoFMg0IAxAAGJIDGIAEGIoFMgwIBBAAGEMYgAQYigUyEAgFEAAYgwEYsQMYgAQYigUyBwgGEAAYgAQyBwgHEAAYgAQyBwgIEAAYgAQyEAgJEAAYgwEYsQMYgAQYigXSAQgxNTAxajBqN6gCALACAA&sourceid=chrome&ie=UTF-8")
+    }
+});
